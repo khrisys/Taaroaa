1e45bd71f929ff66b1a3e39f0034015237e71297
admin <khrisys@laposte.net>
2018-04-25 19:14:02 +0200

    code graphique du covoiturage (ecran passager).
    ref #3
    close #3
    
    Signed-off-by: admin <khrisys@laposte.net>

--- app/src/main/java/fr/drochon/christian/taaroaa/auth/AccountCreateActivity.java
+++ app/src/main/java/fr/drochon/christian/taaroaa/auth/AccountCreateActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
+import android.text.Html;
 import android.text.InputType;
 import android.util.Log;
 import android.view.Menu;
