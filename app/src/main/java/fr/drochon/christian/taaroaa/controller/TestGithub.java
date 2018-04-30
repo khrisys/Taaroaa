c81636138699b0dc4715c322cb748473422b0bf6
admin <khrisys@laposte.net>
2018-04-30 11:46:49 +0200

    commit testgithub et gestion de reference ref #5 close https://github.com/khrisys/Taaroaa/projects/2#card-9376750
    Signed-off-by: admin <khrisys@laposte.net>

--- app/src/main/java/fr/drochon/christian/taaroaa/controller/TestGithub.java
+++ app/src/main/java/fr/drochon/christian/taaroaa/controller/TestGithub.java
@@ -5,5 +5,6 @@ public class TestGithub {
     public TestGithub() {
         System.out.println("test");
         System.out.println("second test our clore une issue");
+        System.out.println("copy card url");
     }
 }
